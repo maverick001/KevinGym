ad6b66d3f029244e55965c3cba2399b7c9803351
kevin bai <k.bai@connect.qut.edu.au>
2026-04-02 17:21:00 +1000

    authentication page working v1

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gym-green': '#2d4a1e',
+        'gym-cream': '#f5f0e8',
+        'gym-tan':   '#e8dcc8',
+      },
+    },
   },
   plugins: [],
 };
